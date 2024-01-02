813a8a0ff9e64d012463f73afd41d8c1103487f2
Roker2 <dimaminko2000@gmail.com>
2024-01-02 18:54:04 +0100

    extensions: JSONDefault: JSONDefault: Migrate to JSONBasic

--- extensions/JSONDefault/JSONDefault.h
+++ extensions/JSONDefault/JSONDefault.h
@@ -27,14 +27,7 @@ THE SOFTWARE.
 #ifndef __SUPPORT_JSONDEFAULT_H__
 #define __SUPPORT_JSONDEFAULT_H__
 
-#include "platform/PlatformMacros.h"
-#include "ExtensionMacros.h"
-#include <string>
-
-#include "yasio/string_view.hpp"
-
-// rapidjson
-#include "rapidjson/document.h"
+#include "extensions/JSONDefault/JSONBasic.h"
 
 /**
  * @addtogroup extensions
@@ -43,7 +36,8 @@ THE SOFTWARE.
 NS_AX_EXT_BEGIN
 
 /**
- * JSONDefault acts as a tiny database. You can save and get base type values by
+ * JSONDefault acts as a tiny database, which is placed in default path
+ * and available everywhere. You can save and get base type values by
  * it. For example, setBoolForKey("played", true) will add a bool value true
  * into the database. Its key is "played". You can get the value of the key by
  * getBoolForKey("played").
@@ -52,189 +46,9 @@ NS_AX_EXT_BEGIN
  * bool, int, float, double, string
  *
  */
-class JSONDefault
+class JSONDefault : public JSONBasic
 {
 public:
-    // get value methods
-
-    /**
-     * Get bool value by key, if the key doesn't exist, will return false.
-     * You can set the default value, or it is false.
-     * @param key The key to get value.
-     * @return Bool value by `key`.
-     * @js NA
-     */
-    bool getBoolForKey(const char* key);
-
-    /**
-     * Get bool value by key, if the key doesn't exist, will return passed default
-     * value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @js NA
-     */
-    virtual bool getBoolForKey(const char* key, bool defaultValue);
-
-    /**
-     * Get integer value by key, if the key doesn't exist, will return 0.
-     * You can set the default value, or it is 0.
-     * @param key The key to get value.
-     * @return Integer value of the key.
-     * @js NA
-     */
-    int getIntegerForKey(const char* key);
-
-    /**
-     * Get integer value by key, if the key doesn't exist, will return passed
-     * default value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @return Integer value of the key.
-     * @js NA
-     */
-    virtual int getIntegerForKey(const char* key, int defaultValue);
-
-    /**
-     * Get large integer value by key, if the key doesn't exist, will return 0.
-     * You can set the default value, or it is 0.
-     * @param key The key to get value.
-     * @return Integer value of the key.
-     * @js NA
-     */
-    int64_t getLargeIntForKey(const char* key);
-
-    /**
-     * Get large integer value by key, if the key doesn't exist, will return
-     * passed default value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @return Integer value of the key.
-     * @js NA
-     */
-    virtual int64_t getLargeIntForKey(const char* key, int64_t defaultValue);
-
-    /**
-     * Get float value by key, if the key doesn't exist, will return 0.0.
-     * @param key The key to get value.
-     * @return Float value of the key.
-     * @js NA
-     */
-    float getFloatForKey(const char* key);
-
-    /**
-     * Get float value by key, if the key doesn't exist, will return passed
-     * default value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @return Float value of the key.
-     * @js NA
-     */
-    virtual float getFloatForKey(const char* key, float defaultValue);
-
-    /**
-     * Get double value by key, if the key doesn't exist, will return 0.0.
-     * @param key The key to get value.
-     * @return Double value of the key.
-     * @js NA
-     */
-    double getDoubleForKey(const char* key);
-
-    /**
-     * Get double value by key, if the key doesn't exist, will return passed
-     * default value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @return Double value of the key.
-     * @js NA
-     */
-    virtual double getDoubleForKey(const char* key, double defaultValue);
-
-    /**
-     * Get string value by key, if the key doesn't exist, will return an empty
-     * string.
-     * @param key The key to get value.
-     * @return String value of the key.
-     * @js NA
-     */
-    std::string_view getStringForKey(const char* key);
-
-    /**
-     * Get string value by key, if the key doesn't exist, will return passed
-     * default value.
-     * @param key The key to get value.
-     * @param defaultValue The default value to return if the key doesn't exist.
-     * @return String value of the key.
-     * @js NA
-     */
-    virtual std::string_view getStringForKey(const char* key, std::string_view defaultValue);
-
-    // set value methods
-
-    /**
-     * Set bool value by key.
-     * @param key The key to set.
-     * @param value A bool value to set to the key.
-     * @js NA
-     */
-    virtual void setBoolForKey(const char* key, bool value);
-    /**
-     * Set integer value by key.
-     * @param key The key to set.
-     * @param value A integer value to set to the key.
-     * @js NA
-     */
-    virtual void setIntegerForKey(const char* key, int value);
-    /**
-     * Set large integer value by key.
-     * @param key The key to set.
-     * @param value A integer value to set to the key.
-     * @js NA
-     */
-    virtual void setLargeIntForKey(const char* key, int64_t value);
-    /**
-     * Set float value by key.
-     * @param key The key to set.
-     * @param value A float value to set to the key.
-     * @js NA
-     */
-    virtual void setFloatForKey(const char* key, float value);
-    /**
-     * Set double value by key.
-     * @param key The key to set.
-     * @param value A double value to set to the key.
-     * @js NA
-     */
-    virtual void setDoubleForKey(const char* key, double value);
-    /**
-     * Set string value by key.
-     * @param key The key to set.
-     * @param value A string value to set to the key.
-     * @js NA
-     */
-    virtual void setStringForKey(const char* key, const char* value);
-
-    /**
-     * Since we reimplement JSONDefault with file mapping io,
-     * you don't needs call this function manually
-     * @param pretty To save json on pretty form.
-     * @js NA
-     */
-    virtual void flush(bool pretty = false);
-
-    /**
-     * Remove all keys and empty data-store,
-     * be carefull, you will lose all your save data.
-     * @js NA
-     */
-    virtual void reset();
-
-    /**
-     * delete any value by key,
-     * @param key The key to delete value.
-     * @js NA
-     */
-    virtual void deleteValueForKey(const char* key);
-
     /** Returns the singleton.
      * @js NA
      * @lua NA
@@ -262,19 +76,8 @@ protected:
     JSONDefault();
     virtual ~JSONDefault();
 
-    void lazyInit();
-
 protected:
-    rapidjson::Document _doc;
-
     static JSONDefault* _jsonDefault;
-    std::string _filePath;
-    bool _initialized = false;
-
-    // encrpyt args
-    bool _encryptEnabled = false;
-    std::string _key;
-    std::string _iv;
 };
 
 NS_AX_EXT_END
